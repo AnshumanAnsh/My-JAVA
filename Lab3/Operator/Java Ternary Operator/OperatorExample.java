b0c829bce4a46b914a9d3c23276da95fcd77439c
Anshuman Dash <anshumandash444@gmail.com>
2024-03-05 21:15:35 -0500

    Initial commit

---
+++ Lab3/Operator/Java Ternary Operator/OperatorExample.java
@@ -0,0 +1,9 @@
+//Java Ternary Operator
+
+public class OperatorExample{  
+public static void main(String args[]){  
+int a=2;  
+int b=5;  
+int min=(a<b)?a:b;  
+System.out.println(min);  
+}}  
---
+++ Lab3/Operator/Logical  and Bitwise/OperatorExample3.java
@@ -0,0 +1,14 @@
+//Java OR Operator Example: Logical || and Bitwise |
+public class OperatorExample3{  
+public static void main(String args[]){  
+int a=10;  
+int b=5;  
+int c=20;  
+System.out.println(a>b||a<c);//true || true = true  
+System.out.println(a>b|a<c);//true | true = true  
+//|| vs |  
+System.out.println(a>b||a++<c);//true || true = true  
+System.out.println(a);//10 because second condition is not checked  
+System.out.println(a>b|a++<c);//true | true = true  
+System.out.println(a);//11 because second condition is checked  
+}}  
